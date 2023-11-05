f2b2adcc80344b64f7919a2906aa75258781edef
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2023-11-05 22:42:57 +0530

    added assets and html css and some js

---
+++ index.js
@@ -0,0 +1,77 @@
+
+var presstheno = document.querySelectorAll(".drum").length;
+
+for ( var i = 0 ; i< presstheno; i++) {
+
+document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+
+
+
+var keyclick = this.innerHTML;
+
+switch (keyclick) {
+    case "w":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+
+     case "a":
+        var kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+        break;
+        
+     case "s":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+        
+     case "d":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();  
+        break;
+
+     case "j":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+
+     case "k":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;   
+
+     case "l":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play(); 
+        break;  
+
+    default:
+        break;
+}
+
+
+});
+}
+/*var audio = new Audio("sounds/tom-1.mp3");
+audio.play();
+
+var numberofclick = document.querySelectorAll(".drum").length;
+
+for ( var i = 0 ; i < numberofclick; i++) {
+
+document.querySelectorAll(".drum")[i].addEventListener("click", press);
+
+function press () {
+
+    alert("play the song");
+    
+};
+}*/
+
+/*document.querySelectorAll(".drum")[1].addEventListener("click", function () {
+
+    alert("stop the song");
+    
+});*/
+
+
